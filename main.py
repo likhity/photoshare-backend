972632b988bf78135273d42dc754f292d6eed14a
likhity <73198499+likhity@users.noreply.github.com>
2023-04-09 00:38:02 -0700

    PSB-4 Hash passwords using bcrypt

--- main.py
+++ main.py
@@ -19,94 +19,95 @@ db_url = os.getenv('DATABASE_URL')
 db_connection = psycopg2.connect(db_url)
 
 CREATE_TABLES = (
-  """
-  CREATE TABLE IF NOT EXISTS Users (
-    userId serial PRIMARY KEY,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(50) NOT NULL,
-    email VARCHAR(320) UNIQUE NOT NULL,
-    homeTown VARCHAR(100),
-    dateOfBirth DATE NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    gender VARCHAR(50),
-    contribution REAL DEFAULT 0,
-    CHECK (gender = 'Male' OR gender = 'Female')
-  );
+    """
+    CREATE TABLE IF NOT EXISTS Users (
+        userId serial PRIMARY KEY,
+        firstName VARCHAR(50) NOT NULL,
+        lastName VARCHAR(50) NOT NULL,
+        email VARCHAR(320) UNIQUE NOT NULL,
+        homeTown VARCHAR(100),
+        dateOfBirth DATE NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        gender VARCHAR(50),
+        contribution REAL DEFAULT 0,
+        CHECK (gender = 'Male' OR gender = 'Female')
+    );
 
-  CREATE TABLE IF NOT EXISTS Albums (
-    albumId serial PRIMARY KEY,
-    AlbumName VARCHAR(50) NOT NULL,
-    ownerId serial,
-    dateOfCreation DATE NOT NULL,
-    FOREIGN KEY (ownerId) REFERENCES Users(userId) ON DELETE CASCADE
-  );
+    CREATE TABLE IF NOT EXISTS Albums (
+        albumId serial PRIMARY KEY,
+        AlbumName VARCHAR(50) NOT NULL,
+        ownerId serial,
+        dateOfCreation DATE NOT NULL,
+        FOREIGN KEY (ownerId) REFERENCES Users(userId) ON DELETE CASCADE
+    );
 
-  CREATE TABLE IF NOT EXISTS Photos (
-    PhotoId serial PRIMARY KEY,
-    caption VARCHAR(100) NOT NULL,
-    albumId serial,
-    filePath VARCHAR(100) NOT NULL,
-    FOREIGN KEY (albumId) REFERENCES Albums(albumId) ON DELETE CASCADE
-  );
+    CREATE TABLE IF NOT EXISTS Photos (
+        PhotoId serial PRIMARY KEY,
+        caption VARCHAR(100) NOT NULL,
+        albumId serial,
+        filePath VARCHAR(100) NOT NULL,
+        FOREIGN KEY (albumId) REFERENCES Albums(albumId) ON DELETE CASCADE
+    );
 
-  CREATE TABLE IF NOT EXISTS Comments (
-    CommentId serial PRIMARY KEY,
-    CommentText VARCHAR(500) NOT NULL,
-    commenterId serial,
-    PhotoId serial,
-    dateOfCreation DATE NOT NULL,
-    FOREIGN KEY (commenterId) REFERENCES Users(userId) ON DELETE CASCADE,
-    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE
-  );
+    CREATE TABLE IF NOT EXISTS Comments (
+        CommentId serial PRIMARY KEY,
+        CommentText VARCHAR(500) NOT NULL,
+        commenterId serial,
+        PhotoId serial,
+        dateOfCreation DATE NOT NULL,
+        FOREIGN KEY (commenterId) REFERENCES Users(userId) ON DELETE CASCADE,
+        FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE
+    );
 
-  CREATE TABLE IF NOT EXISTS Likes (
-    userId serial,
-    PhotoId serial,
-    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
-    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
-    PRIMARY KEY (userId, PhotoId)
-  );
+    CREATE TABLE IF NOT EXISTS Likes (
+        userId serial,
+        PhotoId serial,
+        FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
+        FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
+        PRIMARY KEY (userId, PhotoId)
+    );
 
-  CREATE TABLE IF NOT EXISTS Tags (
-    TagId serial PRIMARY KEY,
-    Tag VARCHAR(50) NOT NULL,
-    PhotoId serial,
-    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
-    CHECK (Tag ~ '^[a-z]+$')
-  );
+    CREATE TABLE IF NOT EXISTS Tags (
+        TagId serial PRIMARY KEY,
+        Tag VARCHAR(50) NOT NULL,
+        PhotoId serial,
+        FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
+        CHECK (Tag ~ '^[a-z]+$')
+    );
 
-  CREATE TABLE IF NOT EXISTS Friends (
-    userId serial,
-    friendId serial,
-    dateOfFriendship DATE NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
-    FOREIGN KEY (friendId) REFERENCES Users(userId) ON DELETE CASCADE,
-    PRIMARY KEY (userId, friendId)
-  );
-  """
+    CREATE TABLE IF NOT EXISTS Friends (
+        userId serial,
+        friendId serial,
+        dateOfFriendship DATE NOT NULL,
+        FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
+        FOREIGN KEY (friendId) REFERENCES Users(userId) ON DELETE CASCADE,
+        PRIMARY KEY (userId, friendId)
+    );
+    """
 )
 
 with db_connection:
-  with db_connection.cursor() as cursor:
-    cursor.execute(CREATE_TABLES)
-    
+    with db_connection.cursor() as cursor:
+        cursor.execute(CREATE_TABLES)
+        
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 def auth_required(func):
-  @wraps(func)
-  def decorated(*args, **kwargs):
-    token = request.cookies.get('jwt')
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('jwt')
+                
+        if not token:
+            return "Unauthorized", 401
         
-    if not token:
-      return "Unauthorized", 401
+        try:
+            data = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
+        except:
+            return "Unauthorized", 401
+        
+        return func(*args, **kwargs)
     
-    try:
-      data = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
-    except:
-      return "Unauthorized", 401
-    
-    return func(*args, **kwargs)
-  return decorated
+    return decorated
 
 import routes.auth_routes
 import routes.friend_routes
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -3,75 +3,81 @@ import time
 import jwt
 from datetime import datetime, timedelta
 from flask import request, jsonify
+import bcrypt
 
 @app.route("/api/time")
 @auth_required
 def get_current_time():
-  return { "time": time.time() }
+    return { "time": time.time() }
 
 @app.post('/api/register')
 def create_user():
-  data = request.get_json()
-  firstName = data["firstName"]
-  lastName = data["lastName"]
-  email = data["email"]
-  homeTown = data["homeTown"]
-  dateOfBirth = data["dateOfBirth"]
-  password = data["password"]
-  
-  INSERT_USER_QUERY = (
-    """
-      INSERT INTO Users (firstName, lastName, email, homeTown, dateOfBirth, password)
-      VALUES (%s, %s, %s, %s, %s, %s)
-      RETURNING userId;
-    """
-  )
-  with db_connection:
-    with db_connection.cursor() as cursor:
-      cursor.execute(INSERT_USER_QUERY, (firstName, lastName, email, homeTown, dateOfBirth, password))
-      userId = cursor.fetchone()[0]
-  
-  # A JWT is valid for 8 hours since first creation
-  jwtMaxAge = 8 * 60 * 60 * 1000
-  
-  token = jwt.encode({
-      'user': userId,
-      'expiration': str(datetime.utcnow() + timedelta(hours=8))
-    }, JWT_SECRET_KEY, algorithm="HS256")
-  
-  response = jsonify({ "id": userId, "message": "User created." })
-  response.set_cookie(key="jwt", value=token, max_age=jwtMaxAge, httponly=True)
-  return response, 201
-
-@app.route("/api/login", methods=['POST'])
-def login():
-  form_data = request.get_json()
-  email = form_data['email']
-  password = form_data['password']
-  
-  SELECT_USER_QUERY = "SELECT userId, email, password FROM Users WHERE email = %s;"
-  with db_connection:
-    with db_connection.cursor() as cursor:
-      cursor.execute(SELECT_USER_QUERY, (email,))
-      result = cursor.fetchone()
-      
-  if result and result[2] == password:
+    data = request.get_json()
+    firstName = data["firstName"]
+    lastName = data["lastName"]
+    email = data["email"]
+    homeTown = data["homeTown"]
+    dateOfBirth = data["dateOfBirth"]
+    password = data["password"]
+    
+    # hash the password
+    hashed_password = bcrypt.hashpw(bytes(password, bcrypt.gensalt())).decode()
+    
+    INSERT_USER_QUERY = (
+        """
+            INSERT INTO Users (firstName, lastName, email, homeTown, dateOfBirth, password)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            RETURNING userId;
+        """
+    )
+    with db_connection:
+        with db_connection.cursor() as cursor:
+            # insert the new user with the hashed password
+            cursor.execute(INSERT_USER_QUERY, (firstName, lastName, email, homeTown, dateOfBirth, hashed_password))
+            userId = cursor.fetchone()[0]
+    
     # A JWT is valid for 8 hours since first creation
     jwtMaxAge = 8 * 60 * 60 * 1000
     
     token = jwt.encode({
-      'user': result[0],
-      'expiration': str(datetime.utcnow() + timedelta(hours=8))
-    }, JWT_SECRET_KEY, algorithm="HS256")
+            'user': userId,
+            'expiration': str(datetime.utcnow() + timedelta(hours=8))
+        }, JWT_SECRET_KEY, algorithm="HS256")
     
-    response = jsonify({ "id": result[0] })
+    response = jsonify({ "id": userId, "message": "User created." })
     response.set_cookie(key="jwt", value=token, max_age=jwtMaxAge, httponly=True)
-    return response
-  else:
-    return { "error": "Authentication failed." }, 401
+    return response, 201
+
+@app.route("/api/login", methods=['POST'])
+def login():
+    form_data = request.get_json()
+    email = form_data['email']
+    password = form_data['password']
+    
+    SELECT_USER_QUERY = "SELECT userId, email, password FROM Users WHERE email = %s;"
+    with db_connection:
+        with db_connection.cursor() as cursor:
+            cursor.execute(SELECT_USER_QUERY, (email,))
+            # result will be an array like so: [userId, email, password]
+            result = cursor.fetchone()
+            
+    if result and bcrypt.checkpw(bytes(password, 'utf-8'), bytes(result[2], 'utf-8')):
+        # A JWT is valid for 8 hours since first creation
+        jwtMaxAge = 8 * 60 * 60 * 1000
+        
+        token = jwt.encode({
+            'user': result[0],
+            'expiration': str(datetime.utcnow() + timedelta(hours=8))
+        }, JWT_SECRET_KEY, algorithm="HS256")
+        
+        response = jsonify({ "id": result[0] })
+        response.set_cookie(key="jwt", value=token, max_age=jwtMaxAge, httponly=True)
+        return response
+    else:
+        return { "error": "Authentication failed." }, 401
 
 @app.route("/api/logout")
 def logout():
-  response = jsonify({ "message": "Logged out." })
-  response.set_cookie(key="jwt", value="", max_age=1)
-  return response
+    response = jsonify({ "message": "Logged out." })
+    response.set_cookie(key="jwt", value="", max_age=1)
+    return response
