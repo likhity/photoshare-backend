d2d93a32a3970b2e4874238a3ca8c83d5e79f7f1
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2023-04-10 12:37:46 -0700

    PSB-16 started work on suggested friends

--- routes/friend_routes.py
+++ routes/friend_routes.py
@@ -2,7 +2,27 @@ from main import app, db_connection, auth_required
 from flask import request
 
 # TODO: PSB-16
+@app.route("/api/suggested-friends", methods=['GET'])
+@auth_required
+def suggested_friends(data):
 
+    #need userId from app? or token
+    print(data)
+    SUGGESTED_FRIENDS_QUERY = (
+        """
+            SELECT friendId, COUNT(*) AS numFriends 
+            FROM Friends 
+            WHERE userId IN 
+                (SELECT friendId FROM Friends WHERE userId = %s) 
+                GROUP BY friendId ORDER BY numFriends DESC;
+        """)
+
+    with db_connection:
+        with db_connection.cursor() as cursor:
+            # retrieve suggested friends (based on ids)
+            cursor.execute(SUGGESTED_FRIENDS_QUERY, ())
+            userId = cursor.fetchone()[0]
+    
 # TODO: PSB-17
 
 # TODO: PSB-19
