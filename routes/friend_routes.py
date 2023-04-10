5ba604d308cbf77747008f41950e97dfb5821cad
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2023-04-10 13:58:08 -0700

    PSB-19 Should work need sample data for testing

--- routes/friend_routes.py
+++ routes/friend_routes.py
@@ -6,3 +6,26 @@ from flask import request
 # TODO: PSB-17
 
 # TODO: PSB-19
+# /api/friends?userId=58
+@app.route("/api/my-friends", methods=['GET'])
+@auth_required
+def my_friends(decoded_token):
+
+    # query for getting all user friends
+    GET_MY_FRIENDS = (
+        """
+            SELECT * 
+            FROM Users 
+            WHERE userId IN (SELECT friendId FROM Friends WHERE userId = %s);
+        """)
+    
+    # get userID from token
+    userID = decoded_token["user"]
+    
+    # execute query and retrieve all friends
+    with db_connection:
+        with db_connection.cursor() as cursor:
+            # retrieve all friends
+            cursor.execute(GET_MY_FRIENDS, (userID))
+            all_my_friends = cursor.fetchall()
+    return all_my_friends, 201
