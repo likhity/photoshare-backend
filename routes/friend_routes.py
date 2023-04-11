fcc6f3da4f6232f53f9bd8011b0185ce4a4e2133
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2023-04-10 18:04:59 -0700

    PSB-19 changed myFriends to user's Friends

--- routes/friend_routes.py
+++ routes/friend_routes.py
@@ -7,25 +7,25 @@ from flask import request
 
 # TODO: PSB-19
 # /api/friends?userId=58
-@app.route("/api/my-friends", methods=['GET'])
-@auth_required
-def my_friends(decoded_token):
+@app.route("/api/friends", methods=['GET'])
+# no authentication needed since we can see everyones friends
+def friends():
 
     # query for getting all user friends
-    GET_MY_FRIENDS = (
+    GET_ALL_FRIENDS = (
         """
             SELECT * 
             FROM Users 
             WHERE userId IN (SELECT friendId FROM Friends WHERE userId = %s);
         """)
     
-    # get userID from token
-    userID = decoded_token["user"]
+    # get userID from url
+    userID = request.args.get("userId")
     
-    # execute query and retrieve all friends
+    # execute query and retrieve all userIds friends
     with db_connection:
         with db_connection.cursor() as cursor:
             # retrieve all friends
-            cursor.execute(GET_MY_FRIENDS, (userID))
-            all_my_friends = cursor.fetchall()
-    return all_my_friends, 201
+            cursor.execute(GET_ALL_FRIENDS, (userID))
+            all_friends = cursor.fetchall()
+    return all_friends, 201
