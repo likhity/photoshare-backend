1f8b0072ffd22e9dd2162ea8ab9dd72c1755286d
likhity <73198499+likhity@users.noreply.github.com>
2023-04-23 23:21:14 -0700

    update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,181 +1,278 @@
--- Names: Joaquin Uribe, Likhit Vyas Yarramsetti, Abhisekhar Bharadwaj Gandavarapu, Alexander Hixson
-
-CREATE TABLE Users (
-  userId serial PRIMARY KEY,
-  firstName VARCHAR(50) NOT NULL,
-  lastName VARCHAR(50) NOT NULL,
-  email VARCHAR(320) UNIQUE NOT NULL,
-  homeTown VARCHAR(100),
-  dateOfBirth DATE NOT NULL,
-  password VARCHAR(50) NOT NULL,
-  gender VARCHAR(50),
-  contribution REAL DEFAULT 0,
-  CHECK (gender = 'Male' OR gender = 'Female')
-);
---create albums table with a foreign key to albums
-CREATE TABLE Albums (
-  albumId serial PRIMARY KEY,
-  AlbumName VARCHAR(50) NOT NULL,
-  ownerId serial,
-  dateOfCreation DATE NOT NULL,
-  FOREIGN KEY (ownerId) REFERENCES Users(userId) ON DELETE CASCADE
+--create tabes
+CREATE TABLE IF NOT EXISTS Users (
+    userId serial PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(320) UNIQUE NOT NULL,
+    homeTown VARCHAR(100),
+    dateOfBirth DATE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    gender VARCHAR(50),
+    contribution REAL DEFAULT 0,
+    CHECK (gender = 'Male' OR gender = 'Female')
 );
 
--- create a photo table with a foreign key to the album table upon deletion of the album, the photos will be deleted as well
-CREATE TABLE Photos (
-  PhotoId serial PRIMARY KEY,
-  caption VARCHAR(100) NOT NULL,
-  albumId serial,
-  filePath VARCHAR(100) NOT NULL,
-  FOREIGN KEY (albumId) REFERENCES Albums(albumId) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS Albums (
+    albumId serial PRIMARY KEY,
+    AlbumName VARCHAR(50) NOT NULL,
+    ownerId serial,
+    dateOfCreation DATE NOT NULL,
+    CONSTRAINT uc_Albums UNIQUE (ownerId, AlbumName),
+    FOREIGN KEY (ownerId) REFERENCES Users(userId) ON DELETE CASCADE
 );
 
--- create a table for the comments with a unique comment id and with following attributes: text, commenter, photoid, and date of comment
-CREATE TABLE Comments (
-  CommentId serial PRIMARY KEY,
-  CommentText VARCHAR(500) NOT NULL,
-  commenterId serial,
-  PhotoId serial,
-  dateOfCreation DATE NOT NULL,
-  FOREIGN KEY (commenterId) REFERENCES Users(userId) ON DELETE CASCADE,
-  FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS Photos (
+    PhotoId serial PRIMARY KEY,
+    caption VARCHAR(100) NOT NULL,
+    albumId serial,
+    filePath VARCHAR(300) NOT NULL,
+    dateOfCreation DATE NOT NULL,
+    FOREIGN KEY (albumId) REFERENCES Albums(albumId) ON DELETE CASCADE
 );
 
---create a table for the likes with a unique like id and with following attributes: userid, photoid
-CREATE TABLE Likes (
-  userId serial,
-  PhotoId serial,
-  FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
-  FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
-  PRIMARY KEY (userId, PhotoId)
+CREATE TABLE IF NOT EXISTS Comments (
+    CommentId serial PRIMARY KEY,
+    CommentText VARCHAR(500) NOT NULL,
+    commenterId serial,
+    PhotoId serial,
+    dateOfCreation DATE NOT NULL,
+    FOREIGN KEY (commenterId) REFERENCES Users(userId) ON DELETE CASCADE,
+    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE
 );
 
---create a table for the tags with a unique tag id and with following attributes: tag (single word and all lower cased with no spaces), photoid. Many photos can belong to the same tag in different albums.also, a tag can be used in multiple photos in the same album check the take is a single word and lowerspaced, with only alphabetic characters
-CREATE TABLE Tags (
-  TagId serial PRIMARY KEY,
-  Tag VARCHAR(50) NOT NULL,
-  PhotoId serial,
-  FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
-  CHECK (Tag ~ '^[a-z]+$')
+CREATE TABLE IF NOT EXISTS Likes (
+    userId serial,
+    PhotoId serial,
+    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
+    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
+    PRIMARY KEY (userId, PhotoId)
 );
 
---create a table for the friends with a unique friend id and with following attributes: userid, friendid, date of friendship. a user can have multiple friends and a friend can have multiple users
-CREATE TABLE Friends (
-  userId serial,
-  friendId serial,
-  dateOfFriendship DATE NOT NULL,
-  FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
-  FOREIGN KEY (friendId) REFERENCES Users(userId) ON DELETE CASCADE,
-  PRIMARY KEY (userId, friendId)
+CREATE TABLE IF NOT EXISTS Tags (
+    TagId serial PRIMARY KEY,
+    Tag VARCHAR(50) NOT NULL,
+    PhotoId serial,
+    FOREIGN KEY (PhotoId) REFERENCES Photos(PhotoId) ON DELETE CASCADE,
+    CHECK (Tag ~ '^[a-z]+$')
 );
 
+CREATE TABLE IF NOT EXISTS Friends (
+    userId serial,
+    friendId serial,
+    dateOfFriendship DATE NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE,
+    FOREIGN KEY (friendId) REFERENCES Users(userId) ON DELETE CASCADE,
+    PRIMARY KEY (userId, friendId)
+);
+    
+SELECT setval('users_userid_seq', (SELECT MAX(userid) FROM Users));
+SELECT setval('albums_albumid_seq', (SELECT MAX(albumId) FROM Albums));
+SELECT setval('photos_photoid_seq', (SELECT MAX(photoid) FROM Photos));
+SELECT setval('comments_commentid_seq', (SELECT MAX(commentid) FROM Comments));
+SELECT setval('tags_tagid_seq', (SELECT MAX(tagid) FROM Tags));
 
-----user management queries below----
--- this query creates a new "friendship" between two users (front end will ensure you cannot friend yourself)
+--select album query
+SELECT albumId, AlbumName, dateOfCreation FROM Albums WHERE ownerId = %s AND AlbumName = %s;
+
+--select albums query
+SELECT albumId, AlbumName, dateOfCreation FROM Albums WHERE ownerId = %s;
+
+--insert album query
+INSERT INTO Albums (AlbumName, ownerId, dateOfCreation) 
+VALUES (%s, %s, %s)
+RETURNING albumId;
+
+--delete album query
+DELETE FROM Albums WHERE ownerId = %s AND AlbumName = %s;
+
+--insert user query
+INSERT INTO Users (firstName, lastName, email, homeTown, dateOfBirth, password, gender)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+RETURNING userId;
+
+--select user for logging in
+SELECT userId, email, password FROM Users WHERE email = %s;
+
+--suggested friends query
+SELECT result.userId, firstName, lastName FROM Users
+JOIN
+(SELECT friendId as userId, COUNT(*) AS numFriends FROM Friends WHERE userId IN (SELECT friendId FROM Friends WHERE userId = 5) GROUP BY friendId ORDER BY numFriends DESC) AS result
+ON Users.userId = result.userid
+ORDER BY result.numFriends DESC;
+
+--create friendship
 INSERT INTO Friends (userId, friendId, dateOfFriendship)
-VALUES (%s, %s, 'yyyy-mm-dd');
+VALUES (%s, %s, %s);
 
-
---register a new user
-INSERT INTO Users (firstName, lastName, email, homeTown, dateOfBirth, password)
-VALUES (%s, %s, %s, %s, %s, %s);
-
---this query searches for a users by name and displays the results in order of last name and date of birth showing hometown and gender if applicable
-SELECT firstName, lastName, dateOfBirth, gender, homeTown FROM Users WHERE firstName = %s AND lastName = %s ORDER BY lastName, dateOfBirth;
-
---display a list of user's friends (self or other) users
-SELECT * FROM Users WHERE userId IN (SELECT friendId FROM Friends WHERE userId = %s);
-
-
-
---display the top 10 users with the contribution points
-SELECT userId, firstName, lastName, contribution FROM Users ORDER BY contribution DESC LIMIT 10;
-
-
-
-
---see if the user exists for logging in
-SELECT userId from Users where email = %s AND password = %s;
---delete user and all albums and photos and comments and likes and tags and friends
-DELETE FROM Users WHERE userId = %s;
-
-----photo/album queries below----
---show all photos for browsing
-SELECT * FROM Photos ORDER BY dateOfCreation DESC;
---show all albums for browsing
-SELECT * FROM Albums ORDER BY dateOfCreation DESC;
-
-
-
---select all photos from an album
-Select * FROM Photos WHERE Albums.albumId = Photos.albumId;
---select album from a photo
-SELECT * FROM Albums where Photos.albumId = Albums.albumId;
-
-
---show all photos for a user/self
-SELECT * FROM Photos WHERE Photos.albumId = Albums.albumId AND Albums.ownerId = %s;
---select all albums for a user/self
-SELECT * FROM Albums WHERE ownerId = %s;
-
-
---select all photos given ownerId
-SELECT * FROM Photos WHERE ownerId = %s;
---select all photos given ownerId and tags
-SELECT * FROM Photos WHERE PhotoId IN (SELECT PhotoId FROM Tags WHERE ownerId = %s AND Tag = %s;
-
-
-
---select album id
+--get all friends
 SELECT 
---create album
-INSERT INTO Albums (AlbumName, ownerId, dateOfCreation) VALUES (%s, %s, 'yyyy-mm-dd');
---upload photo to album and increase contribution
-INSERT INTO Photos (caption, albumId, filePath) VALUES (%s, %s, %s);
+    Users.userId, 
+    Users.firstName, 
+    Users.lastName, 
+    Friends.dateOfFriendship
+FROM 
+    Friends 
+    JOIN Users ON Friends.friendId = Users.userId
+WHERE 
+    Friends.userId = %s;
+
+--get popular tags
+SELECT Tag, COUNT(*) AS numPosts
+FROM Tags 
+GROUP BY Tag 
+ORDER BY COUNT(*) DESC LIMIT 50;
+
+--top 10 users query
+SELECT userId, firstName, lastName, contribution 
+FROM Users 
+ORDER BY contribution 
+DESC LIMIT 10;
+
+--you may also like recommendations
+SELECT *
+FROM Photos
+WHERE PhotoId IN (SELECT photoID FROM
+(SELECT PhotoId, COUNT(*) AS numTags 
+FROM Tags 
+WHERE Tag IN (SELECT Tag 
+FROM Tags 
+WHERE PhotoId IN (SELECT PhotoId 
+FROM Photos 
+WHERE albumId IN 
+(SELECT albumId 
+FROM Albums 
+WHERE ownerId = %s))) 
+GROUP BY PhotoId ORDER BY numTags DESC LIMIT 5) AS result)
+
+-- search by first name
+SELECT * 
+FROM Users
+WHERE firstName LIKE %s;
+
+--search by first and last name
+SELECT *
+FROM Users
+WHERE firstName = %s AND lastName LIKE %s;
+
+--search by first and last name with exact match
+SELECT *
+FROM Users
+WHERE firstName = %s AND lastName = %s;
+
+
+SELECT *
+FROM Users
+WHERE firstName = %s AND lastName = %s AND CAST(userId AS TEXT) LIKE %s;
+
+SELECT * FROM Users WHERE userId = %s
+
+--add photo query
+INSERT INTO Photos (albumId, caption, filePath, dateOfCreation)
+VALUES ((SELECT albumId FROM Albums WHERE ownerId = %s AND AlbumName = %s), %s, %s, %s)
+RETURNING photoId, caption, albumId, filePath, dateOfCreation;
+
+-- updates contribution score
+UPDATE Users SET contribution = contribution + 1 WHERE userId = %s
+
+-- inserts tags
+INSERT INTO Tags (Tag, PhotoId)
+VALUES (%s, %s);
+
+-- insert likes query
+INSERT INTO Likes (userId, PhotoId) 
+VALUES (%s, %s)
+
+-- delete like query
+DELETE FROM Likes WHERE userid = %s AND photoid = %s;
+
+-- insert comments
+INSERT INTO Comments (CommentText, commenterId, PhotoId, dateOfCreation) VALUES (%s, %s, %s, %s);
+--update contribution
 UPDATE Users SET contribution = contribution + 1 WHERE userId = %s;
---delete album and all photos in album
-DELETE FROM Albums WHERE albumId = %s;
---delete photo and all likes and comments on photo
-DELETE FROM Photos WHERE PhotoId = %s;
 
---modify album name if owned by user (will be supplied by ui/front end)
-UPDATE Albums SET AlbumName = %s WHERE albumId = %s AND ownerId = %s;
+-- gets all photos
+SELECT * 
+FROM Photos 
+ORDER BY dateOfCreation DESC;
 
-----tag queries below----
---search photos by tag
-SELECT * FROM Photos WHERE PhotoId IN (SELECT PhotoId FROM Tags WHERE Tag = %s);
+-- all photos given ids
+SELECT * 
+FROM Photos 
+WHERE albumId IN 
+    (SELECT albumId 
+    FROM Albums 
+    WHERE ownerId = %s);
 
---search tags
-SELECT DISTINCT * FROM Tags WHERE  Tag = %s;
+-- all photos given tags and userid
+SELECT Photos.*, COUNT(Tags.Tag) as MatchedTags
+FROM Photos
+JOIN Albums ON Albums.albumId = Photos.albumId
+JOIN Tags ON Tags.PhotoId = Photos.photoId
+WHERE Albums.ownerId = %s
+AND Tags.Tag = ANY(%s)
+GROUP BY Photos.photoId
+ORDER BY MatchedTags DESC;
 
---display all user's photos that have been tagged with a specific tag (will be supplied by ui/front end for self display)
-SELECT * FROM Photos WHERE PhotoId IN (SELECT PhotoId FROM Tags WHERE Tag = %s) AND Photos.albumId = Albums.albumId AND Albums.ownerId = %s;
+--photos given tag and username
+SELECT *
+FROM photos p
+INNER JOIN albums a ON p.albumid = a.albumid
+INNER JOIN users u ON a.ownerid = u.userid
+WHERE a.albumname = %s AND u.userid = %s;
 
---select all photos with a specific tag
-SELECT * FROM Photos WHERE PhotoId IN (SELECT PhotoId FROM Tags WHERE Tag = %s);
---select most popular tags
-SELECT Tag, COUNT(*) FROM Tags GROUP BY Tag ORDER BY COUNT(*) DESC LIMIT 50;
---search all user's photos that have multiple tags
-SELECT * FROM Photos WHERE PhotoId IN (SELECT PhotoId FROM Tags GROUP BY PhotoId HAVING COUNT(*) > 1) AND Photos.albumId = Albums.albumId AND Albums.ownerId = %s;
+-- select individial photo
+SELECT filePath FROM Photos 
+WHERE albumId IN (SELECT albumId FROM Albums WHERE ownerId = %s) 
+AND photoId = %s;
+--delete photo
+DELETE FROM Photos WHERE photoId = %s;
+UPDATE Users SET contribution = contribution - 1 WHERE userId = %s;
+--search comments
+SELECT Comments.*, Users.firstName, Users.lastName
+FROM Comments JOIN Users ON Comments.commenterId = Users.userId
+WHERE photoId = %s AND CommentText LIKE %s
+--select comments
+SELECT Comments.*, Users.firstName, Users.lastName
+FROM Comments JOIN Users ON Comments.commenterId = Users.userId
+WHERE photoId = %s
 
 
---comments/like queries below--
+--get all photo information
+SELECT 
+    Photos.PhotoId,
+    Photos.caption,
+    Photos.albumId,
+    Photos.filePath,
+    Users.firstName,
+    Users.lastName,
+    Users.userId AS ownerId,
+    COUNT(Likes.PhotoId) AS numLikes,
+    ARRAY_AGG(DISTINCT(Tags.Tag)) AS tags,
+    Photos.dateOfCreation
+FROM 
+    Photos
+    JOIN Albums ON Photos.albumId = Albums.albumId
+    JOIN Users ON Albums.ownerId = Users.userId
+    LEFT JOIN Likes ON Photos.PhotoId = Likes.PhotoId
+    LEFT JOIN Tags ON Photos.PhotoId = Tags.PhotoId
+WHERE 
+    Photos.PhotoId = %s
+GROUP BY 
+    Photos.PhotoId,
+    Photos.caption,
+    Photos.albumId,
+    Photos.filePath,
+    Users.firstName,
+    Users.lastName,
+    Users.userId;
 
---add comment to photo and update user's contribution (contrib)
-INSERT INTO Comments (CommentText, commenterId, PhotoId, dateOfCreation) VALUES (%s, %s, %s, 'yyyy-mm-dd');
-UPDATE Users SET contrib = contrib + 1 WHERE userId = %s;
---like a photo and update photo's like count
+--get number of likes for photo
+SELECT COUNT(*)
+FROM Likes
+WHERE photoid = %s;
 
-INSERT INTO Likes (userId, PhotoId) VALUES (%s, %s);
+--see if user already liked the photo
+SELECT COUNT(*)
+FROM Likes
+WHERE photoId = %s AND userId = %s;
 
-
---retrieve like count for a photo
-SELECT COUNT(*) FROM Likes WHERE PhotoId = %s;
---search comments of users by text search. users with the most relevant comments will be displayed first down to the least relevant(how to determine relevancy???)
-SELECT * FROM Comments WHERE CommentText LIKE %s ORDER BY dateOfCreation DESC;
------recommendations queries below-----
---get list of friends of friends ordered by how many times each reccomended friend appears in the list
-SELECT friendId, COUNT(*) AS numFriends FROM Friends WHERE userId IN (SELECT friendId FROM Friends WHERE userId = %s) GROUP BY friendId ORDER BY numFriends DESC;
-
---get list of reccommended photos by taking the 5 most common tags of the user's photos, ordered by how many tags mactch the photo
-SELECT PhotoId, COUNT(*) AS numTags FROM Tags WHERE Tag IN (SELECT Tag FROM Tags WHERE PhotoId IN (SELECT PhotoId FROM Photos WHERE albumId IN (SELECT albumId FROM Albums WHERE ownerId = %s))) GROUP BY PhotoId ORDER BY numTags DESC LIMIT 5;
